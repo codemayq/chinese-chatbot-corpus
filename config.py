a3f4ea5b4526b724c1db51e713e0165ea0b832b7
codingma <mayongqiang@corp.netease.com>
2021-07-04 20:41:19 +0800

    修正PTT 语料缺失问题

--- config.py
+++ config.py
@@ -5,11 +5,12 @@ class Config(object):
     encoding = "utf-8"
 
     clean_chat_corpus_root = "clean_chat_corpus"
-    raw_chat_corpus_root = "raw_chat_corpus"
+    raw_chat_corpus_root = "/Users/codingma/Downloads/raw_chat_corpus"
 
     raw_chatterbot_corpus_root = os.path.join(raw_chat_corpus_root, "chatterbot-1k", "chinese")
     raw_douban_corpus_root = os.path.join(raw_chat_corpus_root, "douban-multiturn-100w")
-    raw_ptt_corpus_path = os.path.join(raw_chat_corpus_root, "ptt-42w", "Gossiping-QA-Dataset.txt")
+    raw_ptt_corpus_path_v1 = os.path.join(raw_chat_corpus_root, "ptt-42w", "Gossiping-QA-Dataset.txt")
+    raw_ptt_corpus_path_v2 = os.path.join(raw_chat_corpus_root, "ptt-77w", "Gossiping-QA-Dataset-2_0.csv")
 
     raw_qingyun_corpus_path = os.path.join(raw_chat_corpus_root, "qingyun-11w", "12万对话语料青云库.csv")
     raw_subtitle_corpus_path = os.path.join(raw_chat_corpus_root, "subtitle-useless", "dgk_shooter_min.conv")
--- process_pipelines/ptt.py
+++ process_pipelines/ptt.py
@@ -1,12 +1,11 @@
 import codecs
 import os
-
+import csv
 from config import Config
 from language.langconv import *
 from util import *
 
-
-def prepocess(raw_corpus_file_name, result_file_name):
+def prepocess_v1(raw_corpus_file_name, result_file_name):
     raw_corpus_file = codecs.open(raw_corpus_file_name, encoding=Config.encoding, errors="replace")
     result_file = codecs.open(result_file_name, "w", encoding=Config.encoding)
 
@@ -26,11 +25,37 @@ def prepocess(raw_corpus_file_name, result_file_name):
     raw_corpus_file.close()
     result_file.close()
 
+def prepocess_v2(raw_corpus_file_name, result_file_name):
+    raw_corpus_file = codecs.open(raw_corpus_file_name, encoding=Config.encoding, errors="replace")
+    result_file = codecs.open(result_file_name, "w", encoding=Config.encoding)
+    csv_raw_corpus = csv.DictReader(raw_corpus_file)
+    for index, line in enumerate(csv_raw_corpus):
+        if index % 100000 == 0:
+            print(raw_corpus_file_name, index)
+        question = line["question"]
+        answer = line["answer"]
+        if "沒有資料" in question or "沒有資料" in answer:
+            continue
+
+        question = tradition2simple(question)
+        answer = tradition2simple(answer)
+
+        result_file.write("\t".join([question, answer]) + "\n")
+
+    raw_corpus_file.close()
+    result_file.close()
+
 
 def ptt_process_pipeline():
     print("ptt_process_pipeline")
 
-    raw_corpus_file_name = Config.raw_ptt_corpus_path
+    raw_corpus_file_name_v1 = Config.raw_ptt_corpus_path_v1
+    raw_corpus_file_name_v2 = Config.raw_ptt_corpus_path_v2
+
     result_file_name = os.path.join(Config.clean_chat_corpus_root, "ptt.tsv")
-    prepocess(raw_corpus_file_name, result_file_name)
-    format_refine(result_file_name)
+    if os.path.exists(raw_corpus_file_name_v1):
+        prepocess_v1(raw_corpus_file_name_v1, result_file_name)
+        format_refine(result_file_name)
+    elif os.path.exists(raw_corpus_file_name_v2):
+        prepocess_v1(raw_corpus_file_name_v2, result_file_name)
+        format_refine(result_file_name)
